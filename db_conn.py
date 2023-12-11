e3a56d8ccca9ae7d2262df4790702919951bd98b
Sathish <69341001+sathish202@users.noreply.github.com>
2023-12-11 18:13:55 +0530

    unwanted commanded lines removed

--- db_conn.py
+++ db_conn.py
@@ -3,13 +3,9 @@ import mysql.connector
 def db_connect(host,user,password, database_name):
     try:
         connection = mysql.connector.connect(host = host, user = user, password = password)
-#        print(host)
- #       print(user)
-  #      print(password)
         if connection.is_connected():
             print("Your are now connected to MySql server")
             cursor = connection.cursor()
-#            database_name = input("Enter the database name: ")
             create_db_query = f"CREATE DATABASE {database_name}"
             cursor.execute(create_db_query)
             print(f"Database '{database_name}' created sucessfully")
@@ -18,4 +14,3 @@ def db_connect(host,user,password, database_name):
             print("MySql server conncetion is close")
     except mysql.connector.Error as error:
         print('Error', error)
-#db_connect(host=input("Enter the host name: "), user=input("Enter the user name: "), password=input("Enter the password: "))
